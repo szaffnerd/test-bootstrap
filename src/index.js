68cdf892a112187efc8a5b4ce0873e8362adf7d7
szaffnerd <szaffnerdaniel@gmail.com>
2019-06-29 23:36:23 +0200

    many changes

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'bootstrap';
+//import css from './style/index.css';
+import './scss/index.scss';
+
+
+
+
+//Adding Carousel images
+//Reference: https://webpack.js.org/guides/asset-management/
+/*import Icon from './icon.png';
+
+
+  function addCarouselImages() {
+    const element = document.createElement('div');
+    element.classList.add('hello');
+
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
+
+    return element;
+  }
+
+  document.body.appendChild(component());
+  */
